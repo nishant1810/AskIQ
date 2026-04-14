f0c1be181a6f42496b00ce2fe11e7c720c2ab89b
Nishant1810 <gaur18nishant@gmail.com>
2026-04-14 11:47:30 +0530

    fix mime + routing properly

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
-import React from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({  
-  plugins: [    
-    React(),tailwindcss(),  
+
+export default defineConfig({
+  base: "/", // 
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
 })
